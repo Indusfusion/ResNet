9a5b9fb1099f784d15e6f8f9735d5845aec9d20f
Indusfusion <105783360+Indusfusion@users.noreply.github.com>
2022-07-05 14:29:52 +0100

    Create Predict.py

---
+++ Predict.py
@@ -0,0 +1,38 @@
+import numpy as np
+import time
+
+import PIL.Image as Image
+import matplotlib.pylab as plt
+
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_hub as hub
+
+import datetime
+import os
+import cv2
+
+
+modelPath = "C:/Users/Robot1/Desktop/FODnew/"
+
+testDataPath = "C:/Users/Robot1/Desktop/FODnew/testData"
+
+model = tf.keras.models.load_model(modelPath)#,custom_objects={'KerasLayer':hub.KerasLayer})
+
+image_size = (400, 400)
+
+dir = listdir(testDataPath)
+
+for i in dir:
+    file = "C:/Users/Robot1/Desktop/FODnew/testData/" + str(i)
+    img = keras.preprocessing.image.load_img(file, target_size=image_size)
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(img_array)
+    predictions = predictions[0]
+    print(predictions)
+    if predictions > 0.5:
+        print("BAD")
+    else:
+        print("GOOD")
