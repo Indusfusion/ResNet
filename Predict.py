5757867103d9286be74a051214174a89f6d590c0
Indusfusion <105783360+Indusfusion@users.noreply.github.com>
2022-07-06 15:33:15 +0100

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -21,12 +21,28 @@ model = tf.keras.models.load_model(modelPath)
 
 image_size = (400, 400)
 
-dir = os.listdir(testDataPath)
+dir0 = os.listdir(testDataPath + "/good")
+dir1 = os.listdir(testDataPath + "/bad")
 
-results = len(dir)
+results = len(dir0) + len(dir1)
+total = len(dir0) + len(dir1)
 
-for i in dir:
-    file = "C:/Users/Robot1/Desktop/FODnew/testData/" + str(i)
+for i in dir0:
+    file = "C:/Users/Robot1/Desktop/FODnew/testData/good/" + str(i)
+    img = keras.preprocessing.image.load_img(file, target_size=image_size)
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(img_array)
+    predictions = predictions[0]
+    if predictions < 0.5:
+        results -= 1
+        print(predictions, "BAD", i)
+    else:
+        print(predictions, "GOOD", i)
+
+for i in dir1:
+    file = "C:/Users/Robot1/Desktop/FODnew/testData/bad/" + str(i)
     img = keras.preprocessing.image.load_img(file, target_size=image_size)
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create batch axis
@@ -36,4 +52,7 @@ for i in dir:
     if predictions < 0.5:
         print(predictions, "BAD", i)
     else:
+        results -= 1
         print(predictions, "GOOD", i)
+        
+  print(f"Results: {results}/{total}    Accuracy: {float(results)/float(total)}%")
