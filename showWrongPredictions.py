b440b78256033e34ebff3d098ed30aa725eb64ee
Indusfusion <thomas.castle@indusfusion.com>
2022-07-07 15:37:08 +0100

    Update showWrongPredictions.py

--- showWrongPredictions.py
+++ showWrongPredictions.py
@@ -2,6 +2,6 @@ import cv2
 
 f = open("C:/Users/Robot1/Desktop/FODnew/Wrongpredictions.txt", 'r')
 lines = f.readlines()
-for line in Lines:
+for line in lines:
     cv2.imshow(line.strip(), line.strip())
     cv2.waitKey(0)
