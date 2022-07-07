550af742818ee5daadfc2fb8baa0fb8d51bf3519
Indusfusion <thomas.castle@indusfusion.com>
2022-07-07 15:36:15 +0100

    Create showWrongPredictions.py

---
+++ showWrongPredictions.py
@@ -0,0 +1,7 @@
+import cv2
+
+f = open("C:/Users/Robot1/Desktop/FODnew/Wrongpredictions.txt", 'r')
+lines = f.readlines()
+for line in Lines:
+    cv2.imshow(line.strip(), line.strip())
+    cv2.waitKey(0)
