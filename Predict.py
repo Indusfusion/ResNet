967d12d8bdb711e433d0da958e1833010a2775a6
Indusfusion <thomas.castle@indusfusion.com>
2022-07-11 15:12:41 +0100

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -13,7 +13,7 @@ import os
 import cv2
 
 
-modelPath = "C:/Users/Robot1/Desktop/FODnew/3ItemModel_v2"
+modelPath = "C:/Users/Robot1/Desktop/FODnew/3ItemModel_v3"
 
 testDataPath = "C:/Users/Robot1/Desktop/FODnew/DATA/Test"
 
